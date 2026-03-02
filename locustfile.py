5e39b4d353443b8d1de74ecce8de615ddbcc3f02
Jainil Gupta <jainilgupta93@gmail.com>
2026-03-03 02:21:37 +0530

    initial commit

---
+++ locustfile.py
@@ -0,0 +1,12 @@
+from locust import HttpUser, task, between
+
+class WebsiteUser(HttpUser):
+    host = "http://jsonplaceholder.typicode.com"
+    wait_time = between(1, 3)
+
+    @task
+    def get_posts(self):
+        self.client.get("/posts")
+    @task
+    def get_one_post(self):
+        self.client.get("/posts/1")
